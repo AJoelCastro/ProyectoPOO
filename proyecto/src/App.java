985156b28c159bfff37cd01a79065b7f6d73e353
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2024-11-29 22:57:40 -0500

    comentario 3

--- proyecto/src/App.java
+++ proyecto/src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello yolver");
-        System.out.println("hola de nuevo");
     }
 }
