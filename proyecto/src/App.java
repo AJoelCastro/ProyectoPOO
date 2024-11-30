ec09bb8415a75f59c538531cea825df3b9750737
yolver50 <aguilarmezayonel50@gmail.com>
2024-11-29 22:57:35 -0500

    comentario 03

--- proyecto/src/App.java
+++ proyecto/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello yolver");
         System.out.println("hola de nuevo");
+        System.out.println("nuevo cambio");
     }
 }
